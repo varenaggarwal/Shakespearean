93aa8def61ec0f120f67420e39eef37fd280371a
Varen Aggarwal <varenaggarwal@gmail.com>
2020-12-17 08:53:48 +0000

    add btn and text area

---
+++ script.js
@@ -0,0 +1,9 @@
+let btnTranslate = document.querySelector("#btn-translate");
+let txtInput = document.querySelector("#userInput");
+
+function clickHandler(){
+  console.log("Clicked!");
+  console.log("Input : " + txtInput.value)
+};
+
+btnTranslate.addEventListener("click" , clickHandler());
