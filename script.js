c265734f4def67034a310794859f415090c91733
Varen Aggarwal <varenaggarwal@gmail.com>
2020-12-18 22:34:16 +0530

    wire the mock server call

--- script.js
+++ script.js
@@ -2,8 +2,22 @@ let btnTranslate = document.querySelector("#btn-translate");
 let txtInput = document.querySelector("#userInput");
 let divOutput = document.querySelector("#output");
 
+let serverUrl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+
+function generateserveQuery(text) {
+  return serverUrl + "?" + "text=" + text;
+}
+
 function clickHandler() {
-  divOutput.innerText = "hehehhe " + txtInput.value;
+  // divOutput.innerText = "hehehhe " + txtInput.value;
+  let userTextInput = txtInput.value;
+  fetch(generateserveQuery(userTextInput))
+    .then((response) => response.json())
+    .then((json) => {
+      let translatedString = json.contents.translated;
+      console.log(translatedString);
+      divOutput.innerText = translatedString;
+    });
 }
 
 btnTranslate.addEventListener("click", clickHandler);
