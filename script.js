e99d270d2ceaf4a5d397477558e08892065c4036
Varen Aggarwal <varenaggarwal@gmail.com>
2020-12-20 09:53:54 +0530

    add style

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ let btnTranslate = document.querySelector("#btn-translate");
 let txtInput = document.querySelector("#userInput");
 let divOutput = document.querySelector("#output");
 
-let serverUrl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
-// let serverUrl = "https://api.funtranslations.com/translate/shakespeare.json"
+// let serverUrl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+let serverUrl = "https://api.funtranslations.com/translate/shakespeare.json"
 
 function errorHandler(error) {
   console.log("Server Errror" + error);
@@ -15,7 +15,6 @@ function generateserveQuery(text) {
 }
 
 function clickHandler() {
-  // divOutput.innerText = "hehehhe " + txtInput.value;
   let userTextInput = txtInput.value;
   fetch(generateserveQuery(userTextInput))
     .then((response) => response.json())
